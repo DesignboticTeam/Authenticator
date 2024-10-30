e1089b256ce2e7f73d4c7286ff01350facfe1c0f
Konrad Zaremba <konradzaremba@gmail.com>
2024-10-30 02:02:29 +0100

    config based

--- Authenticator/Main.cs
+++ Authenticator/Main.cs
@@ -2,6 +2,8 @@
 {
     public static class Main
     {
-
+        //TODO setup Icon and titles throu config
+        //TODO inject after login logic
+        //TODO inverse contorol or extenral control of needed modals -> can be same modal nav
     }
 }
