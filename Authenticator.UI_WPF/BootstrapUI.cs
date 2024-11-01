1ecee2999eaf56e0c2845278ea6d9cf2baf4c97b
Konrad Zaremba <konradzaremba@gmail.com>
2024-11-01 17:13:54 +0100

    json based

---
+++ Authenticator.UI_WPF/BootstrapUI.cs
@@ -0,0 +1,45 @@
+﻿using Authenticator.Components;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI_WPF.Containers;
+using UI_WPF.Services;
+
+namespace Authenticator.UI_WPF
+{
+    public class BootstrapUI
+    {
+        public static IServiceCollection ConfigureAuthentication(IServiceCollection services)
+        {
+            //Check if navigation is added previously
+            if(!services.Any(x => x.GetType() == typeof(NavigationModalService))){
+                services.AddSingleton<NavigationModalService>();
+            }
+
+            if (!services.Any(x => x.GetType() == typeof(NavigationModalService)))
+            {
+                services.AddSingleton<ModalContainer>();
+            }
+
+            if (!services.Any(x => x.GetType() == typeof(NavigationService)))
+            {
+                services.AddSingleton<NavigationService>();
+            }
+
+            if (!services.Any(x => x.GetType() == typeof(PageContainer)))
+            {
+                services.AddSingleton<PageContainer>();
+            }
+
+            services.AddTransient<ModalLoginViewModel>();
+            services.AddTransient<ModalRegisterViewModel>();
+            services.AddTransient<ModalRegisteredViewModel>();
+            services.AddTransient<ModalPasswordChangedViewModel>();
+
+            return services;
+        }
+    }
+}
---
+++ Authenticator/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+using Connector.DataModels;
+using Microsoft.Extensions.Options;
+
+namespace Authenticator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //SettingsAndProps?
+        public MainWindow(IOptions<AppSettings> appSettings)
+        {
+            InitializeComponent();
+            this.Icon = BitmapFrame.Create(new Uri(appSettings.Value.FullIconPath));
+        }
+    }
+}
